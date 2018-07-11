ee32fc71524d3a28fa91875d0c0bde9ae75faf6a
GRAM <gr@m-gan.sl>
2018-07-11 14:45:12 -0500

    adding tpot

--- tpot/first_test.py
+++ tpot/first_test.py
@@ -1,6 +1,9 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+from tpot import TPOTClassifier
 
 digits = datasets.load_digits()
 X = digits.data
@@ -10,4 +13,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
 
 clf = LogisticRegression()
 clf.fit(X_train, y_train)
-clf.predict(X_test)
+accuracy_score(y_test, clf.predict(X_test))
+
+tpot = TPOTClassifier(generations=5, population_size=20, verbosity=2, n_jobs=-1)
+tpot.fit(X_train, y_train)
+print(tpot.score(X_test, y_test))
